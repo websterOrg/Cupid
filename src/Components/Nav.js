a183fac995b3581cc70caae0cc79d3112b1eacee
Man Esh <manishsheela902@gmail.com>
2022-10-26 22:27:14 +0530

    device responsiveness compatibility

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { useState,useEffect,useRef } from "react";
 const Nav = () => {
+  const [openDrawer, toggleDrawer] = useState(false);
+  const drawerRef = useRef(null);
+
+  useEffect(() => {
+    /* Close the drawer when the user clicks outside of it */
+    const closeDrawer = (event) => {
+      if (drawerRef.current && drawerRef.current.contains(event.target)) {
+        return;
+      }
+
+      toggleDrawer(false);
+    };
+
+    document.addEventListener("mousedown", closeDrawer);
+    return () => document.removeEventListener("mousedown", closeDrawer);
+  }, []);
+
+  const DrawerStyle = {
+    background:"white",
+    position: "fixed",
+    display:"flex",
+    padding:"30px 40px",
+    top: "0",
+    right: "0",
+    height: "100vh",
+    boxShadow:" rgba(0, 0, 0, 0.24) 0px 3px 8px",
+    flexDirection: "column",
+    gap: "50px",
+    transition: " 0.5s ease-out",
+  };
   return (
     <nav>
       <div className="logo">
@@ -7,7 +37,7 @@ const Nav = () => {
       </div>
 
       <div className="menu">
-        <ul>
+        <ul ref={drawerRef}  style={openDrawer ? DrawerStyle : null}>
           <li>
             <a href="">Home</a>
           </li>
@@ -26,13 +56,13 @@ const Nav = () => {
         </ul>
       </div>
 
-      <div className="active-bar">
+      <div className="active-bar" onClick={() => toggleDrawer(true)}>
         <span></span>
         <span></span>
         <span></span>
       </div>
     </nav>
   );
-}
+};
 
-export default Nav
+export default Nav;
