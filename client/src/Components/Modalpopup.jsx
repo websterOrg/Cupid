ecc0f55a6162f832f43eb53ad56bc2e4bb11726b
Man Esh <manishsheela902@gmail.com>
2022-10-28 11:25:41 +0530

    connect regsiter and login button with modalpopup componenet

--- client/src/Components/Modalpopup.jsx
+++ client/src/Components/Modalpopup.jsx
@@ -1,62 +1,107 @@
-import React from 'react'
+import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { BsFacebook } from "react-icons/bs";
 import { ImCross } from "react-icons/im";
 
-const Modalpopup = () => {
+const Modalpopup = ({ setShowModal, isSignup, setIsSignup }) => {
+  const handleClick = () => {
+    setShowModal(false);
+  };
+ 
   return (
     <>
-      <div className="modal-container">
-        {/* --------- Cross icon for closing popup model -------- */}
+      <div className="outside-container">
+        <div
+          className="modal-container"
+          style={!isSignup ? { flexDirection: "row-reverse" } : null}
+        >
+          {/* --------- Cross icon for closing popup model -------- */}
 
-        <ImCross className="cross-icon" />
+          <ImCross className="cross-icon" onClick={handleClick} />
 
-        <div className="side-box">
-          <img src="./images/Conversation-rafiki.svg" alt="" />
-        </div>
-        <div className="form-container">
-          <h2>Create Account</h2>
-          <div className="social-icons">
-            <span>
-              {/* ----------Google icon ------ */}
-              <FcGoogle className="google-icon" />
-            </span>
-            <span>
-              {/* ---------FaceBook icon -------- */}
-              <BsFacebook className="facebook-icon" />
-            </span>
+          <div
+            className="side-box"
+            style={
+              !isSignup ? { marginLeft: "20px", marginRight: "-150px" } : null
+            }
+          >
+            <img
+              src={
+                isSignup
+                  ? "./images/Conversation-rafiki.svg"
+                  : "./images/happy_couple.svg"
+              }
+              alt=""
+              style={
+                !isSignup ? { top: "60%" } : null
+              }
+            />
           </div>
-          <p>or use your email for registration!</p>
 
-          <form action="">
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row-reverse",
-                justifyContent: "flex-end",
-                alignItems: "center",
-                gap: "10px",
-                marginTop: "10px",
-              }}
-            >
-              <span className="term-condition">
-                i agree to the
-                <a href=""> Terms </a>
-                and
-                <a href=""> Privacy Policy </a>
+          <div className="form-container">
+            <h2>{isSignup ? "Create Account" : "Log In"}</h2>
+            <div className="social-icons">
+              <span>
+                {/* ----------Google icon ------ */}
+                <FcGoogle className="google-icon" />
               </span>
-              <input type="checkbox" />
+              <span>
+                {/* ---------FaceBook icon -------- */}
+                <BsFacebook className="facebook-icon" />
+              </span>
             </div>
-            <input type="submit" value="Sign Up" />
-          </form>
+
+            <p>
+              or use your email for {isSignup ? "registration!" : "sign in!"}{" "}
+            </p>
+
+            <form action="">
+              <input type="email" placeholder="Email" id="email" name="Name" />
+
+              <input
+                type="password"
+                placeholder="Password"
+                id="password"
+                name="Password"
+              />
+
+              {isSignup && (
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  id="confirmPassword"
+                  name="ConfirmPassword"
+                />
+              )}
+
+              {isSignup && (
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row-reverse",
+                    justifyContent: "flex-end",
+                    alignItems: "center",
+                    gap: "10px",
+                    marginTop: "10px",
+                  }}
+                >
+                  <span className="term-condition">
+                    i agree to the
+                    <a href=""> Terms </a>
+                    and
+                    <a href=""> Privacy Policy </a>
+                  </span>
+                  <input type="checkbox" />
+                </div>
+              )}
+
+              <input type="submit" />
+            </form>
+          </div>
         </div>
       </div>
     </>
   );
-}
+};
 
-export default Modalpopup
+export default Modalpopup;
