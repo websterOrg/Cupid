c17cca54d6307fd8f091d65f32249782c079d74f
Man Esh <manishsheela902@gmail.com>
2022-11-09 12:09:03 +0530

    adding some dependency and form component created

--- client/src/Pages/Form.jsx
+++ client/src/Pages/Form.jsx
@@ -1,101 +1,212 @@
-import React, {useState} from 'react';
-import { MdOutlineArrowBackIosNew } from "react-icons/md";
+// import Nav from "../components/Nav";
+import { useState } from "react";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-export default function Form() {
-  const [page, setPage] = useState(0);
-  const formProgressStage = ["Which best describes you?", "I am interested in..", "My age is", "Bio:", "My all time favourite show has to be:"];
-  const [showMore, setShowMore] = useState(false);
-  
-  const handleNextStep = () => {
-    if(page === formProgressStage.length - 1) alert("Form Submitted");
-    else setPage((cur) => cur + 1);
-  }
+const OnBoarding = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const [formData, setFormData] = useState({
+    user_id: cookies.UserId,
+    first_name: "",
+    dob_day: "",
+    dob_month: "",
+    dob_year: "",
+    show_gender: false,
+    gender_identity: "man",
+    gender_interest: "woman",
+    url: "",
+    about: "",
+    matches: [],
+  });
 
-  const handlePrevStep = () => {
-    setPage((cur) => cur - 1);
-  }
+  let navigate = useNavigate();
 
-  const formPage = () => {
-    if(page === 0) {
-      return (
-        <div className='gender--options'>
-          <button className='options' onClick={handleNextStep}> Male </button>
-          <button className='options' onClick={handleNextStep}> Female </button>
-          {!showMore && <button className='show-btn' onClick={() => setShowMore(true)}>More Options </button>}
-          {
-            showMore && <span className='more--options'>
-              <button className='options' onClick={handleNextStep}> Agender </button>
-              <button className='options' onClick={handleNextStep}> Androgynous </button>
-              <button  className='options' onClick={handleNextStep}> Bigender </button>
-              <button className='options' onClick={handleNextStep}> Demiman </button>
-              <button className='options' onClick={handleNextStep}> Demiwoman </button>
-              <button className='options' onClick={handleNextStep}> Gender Fluid </button>
-              <button className='options' onClick={handleNextStep}> Gender Nonconforming </button>
-              <button className='options' onClick={handleNextStep}> Gender Questioning </button>
-              <button className='options' onClick={handleNextStep}> Gender Varient </button>
-              <button className='options' onClick={handleNextStep}> Genderqueer </button>
-              <button className='options' onClick={handleNextStep}> Intersex </button>
-              <button className='options' onClick={handleNextStep}> Non-Binary </button>
-              <button className='options' onClick={handleNextStep}> Other </button>
-              <button className='options' onClick={handleNextStep}> Pangender </button>
-              <button className='options' onClick={handleNextStep}> Polygender </button>
-              <button className='options' onClick={handleNextStep}> Trans Man </button>
-              <button className='options' onClick={handleNextStep}> Trans Person </button>
-              <button className='options' onClick={handleNextStep}> Trans Woman </button>
-              <button className='options' onClick={handleNextStep}> Transgender </button>
-              <button className='options' onClick={handleNextStep}> Transsexual </button>
-            </span>
-          }
-        </div>
-      )
+  const handleSubmit = async (e) => {
+    console.log("submitted");
+    e.preventDefault();
+    try {
+      const response = await axios.put("http://localhost:8000/user", {
+        formData,
+      });
+      console.log(response);
+      const success = response.status === 200;
+      if (success) navigate("/dashboard");
+    } catch (err) {
+      console.log(err);
     }
-    else if(page === 1) {
-      return (
-        <div>
-          <button className='options' onClick={handleNextStep}> Male </button>
-          <button className='options' onClick={handleNextStep}> Female </button>
-          <button className='options' onClick={handleNextStep}> Everyone </button>
-        </div>
-      )
-    }
-    else if(page === 2) {
-      return (
-        <>
-        <input type='number' />
-        <button className='options' onClick={handleNextStep}>Thats it</button>
-        </>
-        )
-      }
-    else if(page === 3) {
-      return (
-        <>
-          <textarea name="bio" cols="30" rows="10" />
-          <button className='options' onClick={handleNextStep}>Next</button>
-        </>
-      )
-    }
-    else if(page === 4) {
-      return (
-        <>
-          <input type='text' />
-          <button className='options' onClick={handleNextStep}>Next</button>
-        </>
-      )
-    }
-    }
+  };
 
+  const handleChange = (e) => {
+    console.log("e", e);
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    const name = e.target.name;
 
-    return (
-    <div>
-      <div className='form--container'>
-        <div className='form--header'>
-          {page > 0 && <MdOutlineArrowBackIosNew className='back-icon' onClick={handlePrevStep}/> }
-          {formProgressStage[page]}
-        </div>
-        <div className='form--body'>
-          {formPage()}
-        </div>
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <>
+      {/* <Nav minimal={true} setShowModal={() => {}} showModal={false} /> */}
+
+      <div className="onboarding">
+        <h2>CREATE ACCOUNT</h2>
+
+        <form onSubmit={handleSubmit}>
+          <section>
+            <label htmlFor="first_name">First Name</label>
+            <input
+              id="first_name"
+              type="text"
+              name="first_name"
+              placeholder="First Name"
+              required={true}
+              value={formData.first_name}
+              onChange={handleChange}
+            />
+
+            <label>Birthday</label>
+            <div className="multiple-input-container">
+              <input
+                id="dob_day"
+                type="number"
+                name="dob_day"
+                placeholder="DD"
+                required={true}
+                value={formData.dob_day}
+                onChange={handleChange}
+              />
+
+              <input
+                id="dob_month"
+                type="number"
+                name="dob_month"
+                placeholder="MM"
+                required={true}
+                value={formData.dob_month}
+                onChange={handleChange}
+              />
+
+              <input
+                id="dob_year"
+                type="number"
+                name="dob_year"
+                placeholder="YYYY"
+                required={true}
+                value={formData.dob_year}
+                onChange={handleChange}
+              />
+            </div>
+
+            <label>Gender</label>
+            <div className="multiple-input-container">
+              <input
+                id="man-gender-identity"
+                type="radio"
+                name="gender_identity"
+                value="man"
+                onChange={handleChange}
+                checked={formData.gender_identity === "man"}
+              />
+              <label htmlFor="man-gender-identity">Man</label>
+              <input
+                id="woman-gender-identity"
+                type="radio"
+                name="gender_identity"
+                value="woman"
+                onChange={handleChange}
+                checked={formData.gender_identity === "woman"}
+              />
+              <label htmlFor="woman-gender-identity">Woman</label>
+              <input
+                id="more-gender-identity"
+                type="radio"
+                name="gender_identity"
+                value="more"
+                onChange={handleChange}
+                checked={formData.gender_identity === "more"}
+              />
+              <label htmlFor="more-gender-identity">More</label>
+            </div>
+
+            <label htmlFor="show-gender">Show Gender on my Profile</label>
+
+            <input
+              id="show-gender"
+              type="checkbox"
+              name="show_gender"
+              onChange={handleChange}
+              checked={formData.show_gender}
+            />
+
+            <label>Show Me</label>
+
+            <div className="multiple-input-container">
+              <input
+                id="man-gender-interest"
+                type="radio"
+                name="gender_interest"
+                value="man"
+                onChange={handleChange}
+                checked={formData.gender_interest === "man"}
+              />
+              <label htmlFor="man-gender-interest">Man</label>
+              <input
+                id="woman-gender-interest"
+                type="radio"
+                name="gender_interest"
+                value="woman"
+                onChange={handleChange}
+                checked={formData.gender_interest === "woman"}
+              />
+              <label htmlFor="woman-gender-interest">Woman</label>
+              <input
+                id="everyone-gender-interest"
+                type="radio"
+                name="gender_interest"
+                value="everyone"
+                onChange={handleChange}
+                checked={formData.gender_interest === "everyone"}
+              />
+              <label htmlFor="everyone-gender-interest">Everyone</label>
+            </div>
+
+            <label htmlFor="about">About me</label>
+            <input
+              id="about"
+              type="text"
+              name="about"
+              required={true}
+              placeholder="I like long walks..."
+              value={formData.about}
+              onChange={handleChange}
+            />
+
+            <input type="submit" />
+          </section>
+
+          <section>
+            <label htmlFor="url">Profile Photo</label>
+            <input
+              type="url"
+              name="url"
+              id="url"
+              onChange={handleChange}
+              required={true}
+            />
+            <div className="photo-container">
+              {formData.url && (
+                <img src={formData.url} alt="profile pic preview" />
+              )}
+            </div>
+          </section>
+        </form>
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
+export default OnBoarding;
